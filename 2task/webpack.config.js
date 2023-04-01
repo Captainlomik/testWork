63846f21b1ae90dee2cccd5ea7984a8bfc2953f3
Captainlomik <nastiy248@mail.ru>
2023-04-01 21:01:20 +0300

    add folder for assets webpack

--- 2task/webpack.config.js
+++ 2task/webpack.config.js
@@ -1,25 +1,28 @@
-const path = require('path');
-const miniCss = require('mini-css-extract-plugin');
+const path = require("path");
+const miniCss = require("mini-css-extract-plugin");
 module.exports = {
-   entry: './src/index.js',
-   output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    mode: 'production', 
-   module: {
-      rules: [{
-         test:/\.(s*)css$/,
-         use: [
-            miniCss.loader,
-            'css-loader',
-            'sass-loader',
-         ]
-      }]
-   },
-   plugins: [
-      new miniCss({
-         filename: 'style.css',
-      }),
-   ]
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/images/[name][ext]",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [miniCss.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new miniCss({
+      filename: "style.css",
+    }),
+  ],
 };
